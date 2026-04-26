efe7ac13307967039909c7cd8d80558e37562661
Ahmed Raza <ahmedrazaposwal@Ahmeds-MacBook-Air.local>
2026-04-26 13:51:55 +0200

    adds register and login api endpoints, additionally, implement password encryption by BCrypt

---
+++ backend/erp-backend/src/main/java/com/arp/erp_backend/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.arp.erp_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String email;
+    private String password;
+}
---
+++ backend/erp-backend/src/main/java/com/arp/erp_backend/service/AuthService.java
@@ -0,0 +1,49 @@
+package com.arp.erp_backend.service;
+
+
+import com.arp.erp_backend.dto.LoginRequest;
+import com.arp.erp_backend.dto.RegisterRequest;
+import com.arp.erp_backend.entity.User;
+import com.arp.erp_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder  passwordEncoder;
+
+    public String register(RegisterRequest request) {
+        // create new user object
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        // encrypt password before saving
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        user.setRole("USER");
+        userRepository.save(user);
+        return "User registered successfully";
+    }
+
+    public String login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        boolean matches = passwordEncoder.matches(
+                request.getPassword(),
+                user.getPassword()
+        );
+
+        if (!matches) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return ("Login successfully");
+    }
+
+}
